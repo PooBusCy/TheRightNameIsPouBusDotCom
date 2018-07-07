c92ee1f386cc35e7f5e60b04611a0174b991991c
savvasal <savvas.alexandrou@gmail.com>
2018-07-08 00:53:26 +0300

    fix the mess with git merge

--- src/Controller/PouNaPawController.php
+++ src/Controller/PouNaPawController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\BusStop;
+use App\Repository\BusStopRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,5 +24,11 @@ class PouNaPawController extends Controller
      */
     public function test(){
         return $this->render('pou_na_paw/aboutus.html.twig');
+    }
+
+    public function getDirections($from, $to)
+    {
+        // find the nearest bus stop from $from
+        // $find the nearest bus stop from $to
     }
 }
