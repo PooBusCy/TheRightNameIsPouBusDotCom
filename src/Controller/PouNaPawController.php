dc1175b33b9419058f4890f4d8c4b6ad5190fcc3
Eleni <ioakim39@gmail.com>
2018-07-07 21:37:19 +0300

    update - 07/07/2018 21:37
    homepage and about us update

--- src/Controller/PouNaPawController.php
+++ src/Controller/PouNaPawController.php
@@ -3,15 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PouNaPawController extends Controller
 {
     /**
-     * @Route("/", name="pou_bus_homepage")
+     * @Route("/")
      */
     public function homepage()
     {
         return $this->render('pou_na_paw/index.html.twig');
+    }
+
+    /**
+     * @Route("/about-us")
+     * @return Response
+     */
+    public function test(){
+        return $this->render('pou_na_paw/aboutus.html.twig');
     }
 }
