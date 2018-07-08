290d0dd05f4f2223dcbc2102e11f93abf9124458
savvasal <savvas.alexandrou@gmail.com>
2018-07-08 12:17:37 +0300

    update

--- src/Controller/PouNaPawController.php
+++ src/Controller/PouNaPawController.php
@@ -7,6 +7,7 @@ use App\Repository\BusStopRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use \RestClient;
 
 class PouNaPawController extends Controller
 {
@@ -25,6 +26,49 @@ class PouNaPawController extends Controller
      */
     public function aboutUs(){
         return $this->render('pou_na_paw/aboutus.html.twig');
+    }
+
+    /**
+     * @Route("/tziame")
+     * @return Response
+     * @throws \RestClientException
+     */
+    public function tziame(){
+        // @todo remove it
+        $apiKey = "AIzaSyC3l5YJo32fjGjdXH6JHZsW5LyFpyz9WHs";
+        $endpoint = "https://maps.googleapis.com/";
+        $origin = "Ledras,+Nicosia";
+        $destination = "Plateia+Solomou,+Nicosia,+Cyprus";
+        $mode = "walking";
+        $navigation_request = "maps/api/directions/json?origin=${origin}&destination=${destination}&key=${apiKey}&mode=${mode}";
+        $api = new RestClient([
+            'base_url' => $endpoint,
+            'format' => "json",
+            // https://dev.twitter.com/docs/auth/application-only-auth
+            // 'headers' => ['Authorization' => 'Bearer '.OAUTH_BEARER],
+        ]);
+        $result = $api->get($navigation_request);
+        var_dump($result->decode_response());
+        // json_decode($response, true)
+        // $resultArray = json_decode($result, true);
+//        $resultArray = (array) $result;
+//        // var_dump($resultArray['response']);
+//        $resultBodyArray = (array) json_decode($resultArray['response']);
+//        $legs = (array) $resultBodyArray['routes'][0];
+        // var_dump($legs['legs'][0]);
+  //      $stepsArray = (array) $legs['legs'][0];
+        // var_dump($stepsArray);
+    //    $steps = $legs['legs'][0]['steps'];
+//        $responseHtml = "<ul>";
+//        foreach ($steps as $step) {
+//            $responseHtml = $responseHtml . "<li>" . $step['html_instructions'] . "</li>";
+//        }
+//        $responseHtml = $responseHtml . "</ul>";
+        // $response = new Response($result->response);
+        // $response->headers->set('Content-Type', 'application/json'
+        //);
+        $steps = [];
+        return $this->render('pou_na_paw/index.html.twig', ['steps' => $steps]);
     }
 
     /**
